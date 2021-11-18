87a9557257e20dc37186a0958081da6ba10a3e0f
Tim Holmes <tim@Tims-MacBook-Air.local>
2021-11-18 15:46:22 -0500

    working login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Auth } from 'aws-amplify';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gift-ideas';
+  isAuthenticated = false;
+
+  ngOnInit(): void {
+    Auth.currentAuthenticatedUser().then(user => {
+      console.log('authenticated? yes');
+      this.isAuthenticated = true;
+      // sessionStorage.setItem('isAuthenticated', 'true');
+      // sessionStorage.setItem('user', user);
+    }).catch(e => {
+      console.log(e);
+      console.log('authenticated? no');
+      this.isAuthenticated = false;
+      // sessionStorage.setItem('isAuthenticated' 'false');
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* Add Amplify imports */
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import Amplify from 'aws-amplify';
+import awsconfig from '../aws-exports';
+
+
+/* Configure Amplify resources */
+Amplify.configure(awsconfig);
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AmplifyUIAngularModule,
     BrowserModule,
     AppRoutingModule
   ],
